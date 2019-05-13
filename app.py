9e08484668e0774e0957ad60a8a711762d3d5952
celinehuang <celine756@hotmail.com>
2019-05-13 17:34:05 -0400

    Correctly formatted using Black

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ from local import local_layout, local_callbacks
 app = dash.Dash(__name__)
 server = app.server
 
-# if 'DYNO' in os.environ:
-#     app.scripts.append_script({
-#         'external_url': 'https://codepen.io/plotly/pen/BGyZNa.js'
-#     })
+if "DYNO" in os.environ:
+    app.scripts.append_script(
+        {"external_url": "https://codepen.io/plotly/pen/BGyZNa.js"}
+    )
 
-demo_mode = True
-# else:
-#     demo_mode = False
+    demo_mode = True
+else:
+    demo_mode = False
 
 
 # App
@@ -37,12 +37,12 @@ external_css = [
     "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css",
     "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
     "//fonts.googleapis.com/css?family=Raleway:400,300,600",
-    "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 ]
 
 for css in external_css:
     app.css.append_css({"external_url": css})
 
 # Running the server
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
